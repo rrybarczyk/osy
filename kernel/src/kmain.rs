10ac20208c56cb438d0bacc016f85c5fdcb1429f
RJ Rybarczyk <rj@rybar.tech>
2019-03-27 14:10:36 -0400

    System timer blinks LED

--- kernel/src/kmain.rs
+++ kernel/src/kmain.rs
@@ -8,6 +8,7 @@
 #![feature(attr_literals)]
 #![feature(never_type)]
 #![feature(ptr_internals)]
+#![feature(pointer_methods)]
 
 extern crate pi;
 extern crate stack_vec;
@@ -17,7 +18,30 @@ pub mod mutex;
 pub mod console;
 pub mod shell;
 
+const GPIO_BASE: usize = 0x3F000000 + 0x200000;
+
+const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
+const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
+const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
+
 #[no_mangle]
 pub extern "C" fn kmain() {
-    // FIXME: Start the shell.
+    let pin = 16;
+    let shift = (pin % 10) * 3;
+    unsafe {
+        GPIO_FSEL1.write_volatile(GPIO_FSEL1.read_volatile() & !(0b111 << shift));
+        GPIO_FSEL1.write_volatile(GPIO_FSEL1.read_volatile() | (0b001 << shift));
+
+        // FIXME: STEP 2: Continuously set and clear GPIO 16.
+        loop {
+            GPIO_SET0.write_volatile(GPIO_SET0.read_volatile() & !(0b1 << pin));
+            GPIO_SET0.write_volatile(GPIO_SET0.read_volatile() | (0b1 << pin));
+            // spin_sleep_ms(500);
+            pi::timer::spin_sleep_ms(5000);
+            GPIO_CLR0.write_volatile(GPIO_CLR0.read_volatile() & !(0b1 << pin));
+            GPIO_CLR0.write_volatile(GPIO_CLR0.read_volatile() | (0b1 << pin));
+            pi::timer::spin_sleep_ms(5000);
+            // spin_sleep_ms(500);
+        }
+    }
 }
