a5e167747591ad3a473a45793a74b150a062ef2a
RJ Rybarczyk <rj@rybar.tech>
2019-04-09 12:26:31 -0400

    Get next ATAG

--- pi/src/atags/raw.rs
+++ pi/src/atags/raw.rs
@@ -7,20 +7,30 @@ pub struct Atag {
 }
 
 impl Atag {
-    pub const NONE: u32 = 0x00000000;
-    pub const CORE: u32 = 0x54410001;
-    pub const MEM: u32 = 0x54410002;
-    pub const VIDEOTEXT: u32 = 0x54410003;
-    pub const RAMDISK: u32 = 0x54410004;
-    pub const INITRD2: u32 = 0x54420005;
-    pub const SERIAL: u32 = 0x54410006;
-    pub const REVISION: u32 = 0x54410007;
-    pub const VIDEOLFB: u32 = 0x54410008;
-    pub const CMDLINE: u32 = 0x54410009;
+    pub const NONE: u32 = 0x00000000;       // Empty rag used to end lst
+    pub const CORE: u32 = 0x54410001;       // Start tag used to begin list
+    pub const MEM: u32 = 0x54410002;        // Tag used to describe a physical area of memory
+    pub const VIDEOTEXT: u32 = 0x54410003;  // Tag used to describe VGA text type displays
+    pub const RAMDISK: u32 = 0x54410004;    // Tag describing how the ramdisk will be used by the kernel
+    pub const INITRD2: u32 = 0x54420005;    // Tag describing the physical location of the compressed ramdisk
+    pub const SERIAL: u32 = 0x54410006;     // Tag with 64 bit serial number of the board
+    pub const REVISION: u32 = 0x54410007;   // Tag for the board revision
+    pub const VIDEOLFB: u32 = 0x54410008;   // Tag describing parameters for a framebuffer type display
+    pub const CMDLINE: u32 = 0x54410009;    // Tag used to pass the commandline to the kernel
 
     /// Returns the ATAG following `self`, if there is one.
+    /// #define tag_next(t)     ((struct tag *)((u32 *)(t) + (t)->hdr.size))
     pub fn next(&self) -> Option<&Atag> {
-        unimplemented!()
+
+        // Cast Atag pointer as u32
+        let tag_current = self as *const Atag as *const u32;
+
+        // Return next ATAG
+        unsafe {
+            // Get next ATAG, returns *const u32 cast as *const Atag
+            let tag_next = tag_current.offset(self.dwords as isize) as *const Atag;
+            tag_next.as_ref()
+        }
     }
 }
 
