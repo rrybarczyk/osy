3c61aa7edf1cc60d27fa6da8c115b690dfe54643
RJ Rybarczyk <rj@rybar.tech>
2019-04-09 12:24:16 -0400

    Format panic

--- kernel/src/lang_items.rs
+++ kernel/src/lang_items.rs
@@ -1,8 +1,23 @@
+use console::{kprintln};
+
 #[no_mangle]
 #[cfg(not(test))]
 #[lang = "panic_fmt"]
 pub extern fn panic_fmt(fmt: ::std::fmt::Arguments, file: &'static str, line: u32, col: u32) -> ! {
-    // FIXME: Print `fmt`, `file`, and `line` to the console.
+    // Print `fmt`, `file`, and `line` to the console.
+    kprintln!("");
+
+    kprintln!("(-(-_(-_-)_-)-)");
+
+    kprintln!("");
+
+    kprintln!("FILE: {}", file);
+    kprintln!("LINE: {}", line);
+    kprintln!("COL:  {}", col);
+
+    kprintln!("");
+
+    kprintln!("{:?}", fmt);
 
     loop { unsafe { asm!("wfe") } }
 }
